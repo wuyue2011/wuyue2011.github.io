4296c66847e1026b871147d348fe601310f42a83
aphrodite281 <Aphrodite520@outlook.com>
2024-08-29 10:50:16 +0800

    MinecraftClientUtil 加方法

--- common/src/main/java/cn/zbx1425/mtrsteamloco/render/scripting/util/MinecraftClientUtil.java
+++ common/src/main/java/cn/zbx1425/mtrsteamloco/render/scripting/util/MinecraftClientUtil.java
@@ -42,4 +42,20 @@ public class MinecraftClientUtil {
             });
         }
     }
+
+    public static void stopAllSound() {
+        Minecraft.getInstance().getSoundManager().stop();
+    }
+
+    public static void stopSound(SoundInstance sound) {
+        Minecraft.getInstance().getSoundManager().stop(sound);
+    }
+
+    public static void stopSound(ResourceLocation sound, SoundSource source) {
+        Minecraft.getInstance().getSoundManager().stop(sound, source);
+    }
+
+    public static void stopSound(ResourceLocation sound){
+        Minecraft.getInstance().getSoundManager().stop(sound, SoundSource.BLOCKS);
+    }
 }
