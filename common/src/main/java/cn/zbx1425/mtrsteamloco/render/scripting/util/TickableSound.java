05d735934e81eb0ddb022bb0f5b1d233c4aab287
aphrodite281 <Aphrodite520@outlook.com>
2024-08-29 10:33:28 +0800

    改package

--- common/src/main/java/cn/zbx1425/mtrsteamloco/render/scripting/util/TickableSound.java
+++ common/src/main/java/cn/zbx1425/mtrsteamloco/render/scripting/util/TickableSound.java
@@ -1,4 +1,4 @@
-package cn.zbx1425.mtrsteamloco.render.scripting.util;
+package cn.zbx1425.mtrsteamloco.test;//render.scripting.util1
 
 import mtr.mappings.TickableSoundInstanceMapper;
 import net.minecraft.client.Minecraft;
