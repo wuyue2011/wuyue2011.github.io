0f2bf862d4b1179675d66db3957257e91d02e3c1
aphrodite281 <Aphrodite520@outlook.com>
2024-08-29 11:54:56 +0800

    加方法

--- common/src/main/java/cn/zbx1425/mtrsteamloco/render/scripting/util/TickableSound.java
+++ common/src/main/java/cn/zbx1425/mtrsteamloco/render/scripting/util/TickableSound.java
@@ -17,17 +17,98 @@ public class TickableSound extends TickableSoundInstanceMapper {
         super(event, source);
     }
 
-    public void setData(float volume, float pitch, BlockPos pos) {
+    public void setData(float volume, float pitch, Vector3f pos) {
 		this.pitch = pitch;
 		if (this.pitch == 0) {
 			this.pitch = 1;
 		}
 		this.volume = volume;
 
-		x = pos.getX();
-		y = pos.getY();
-		z = pos.getZ();
+		x = pos.x();
+		y = pos.y();
+		z = pos.z();
 	}
+
+    
+
+    public void setVolume(float volume1){
+        volume = volume1;
+    }
+
+    public void setPitch(float pitch1){
+        pitch = pitch1;
+    }
+
+    public void play(){
+        Minecraft.getInstance().getSoundManager().play(this);
+    }
+
+    public void setData(float volume1, float pitch1, Vector3f position, boolean looping1, int delay1, boolean relative1){
+        volume = volume1;
+        pitch = pitch1;
+        x = position.x();
+        y = position.y();
+        z = position.z();
+        looping = looping1;
+        delay = delay1;
+        relative = relative1;
+    }
+
+    public void setData(float volume1, float pitch1, double x1, double y1, double z1, boolean looping1, int delay1, boolean relative1){
+        volume = volume1;
+        pitch = pitch1;
+        x = x1;
+        y = y1;
+        z = z1;
+        looping = looping1;
+        delay = delay1;
+        relative = relative1;
+    }
+
+    public void setData(float volume1, float pitch1){
+        volume = volume1;
+        pitch = pitch1;
+    }
+
+    public void setPosition(Vector3f position){
+        x = position.x();
+        y = position.y();
+        z = position.z();
+    }
+
+    public void setPosition(double x1, double y1, double z1){
+        x = x1;
+        y = y1;
+        z = z1;
+    }
+
+    public void setX(double x1){
+        x = x1;
+    }
+
+    public void setY(double y1){
+        y = y1;
+    }
+
+    public void setZ(double z1){
+        z = z1;
+    }
+
+    public void setLooping(boolean looping1){
+        looping = looping1;
+    }
+
+    public void setDelay(int delay1){
+        delay = delay1;
+    }
+
+    public void setRelative(boolean relative1){
+        relative = relative1;
+    }
+
+    public void setStoped(boolean stoped1){
+        stoped = stoped1;
+    }
 
     public void play(){
         Minecraft.getInstance().getSoundManager().play(this);
