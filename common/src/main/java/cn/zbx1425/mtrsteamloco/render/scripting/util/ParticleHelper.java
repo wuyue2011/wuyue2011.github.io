7c52543f99a30425a1dfd980afc1fe77c2f26470
aphrodite281 <Aphrodite520@outlook.com>
2024-09-01 21:33:03 +0800

    添加 ParticleHelper

---
+++ common/src/main/java/cn/zbx1425/mtrsteamloco/render/scripting/util/ParticleHelper.java
@@ -0,0 +1,14 @@
+package cn.zbx1425.mtrsteamloco.render.scripting.util;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.core.particles.ParticleOptions;
+import cn.zbx1425.sowcer.math.Vector3f;
+
+public class ParticleHelper {
+    public static void addParticle(ParticleOptions particle, Vector3f pos, Vector3f speed){
+        Minecraft.getInstance().execute(() -> {
+            Minecraft.getInstance().level.addParticle(particle, pos.x(), pos.y(), pos.z(), speed.x(), speed.y(), speed.z());
+        });
+    }
+}
