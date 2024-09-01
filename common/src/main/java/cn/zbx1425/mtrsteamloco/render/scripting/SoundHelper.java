6ad2aebc8a0ca9128ee430d5cba2d43017d91d55
aphrodite281 <Aphrodite520@outlook.com>
2024-09-01 20:10:47 +0800

    添加 SoundHelper

---
+++ common/src/main/java/cn/zbx1425/mtrsteamloco/render/scripting/SoundHelper.java
@@ -0,0 +1,70 @@
+package cn.zbx1425.mtrsteamloco.render.scripting;
+
+import cn.zbx1425.sowcer.math.Vector3f;
+import com.mojang.text2speech.Narrator;
+import mtr.mappings.Text;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.sounds.SoundInstance;
+import net.minecraft.client.sounds.SoundManager;
+import net.minecraft.commands.synchronization.brigadier.StringArgumentSerializer;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.entity.player.Player;
+
+public class SoundHelper {
+
+    public static boolean worldIsRaining() {
+        return Minecraft.getInstance().level != null
+                && Minecraft.getInstance().level.isRaining();
+    }
+
+    public static boolean worldIsRainingAt(Vector3f pos) {
+        return Minecraft.getInstance().level != null
+                && Minecraft.getInstance().level.isRainingAt(pos.toBlockPos());
+    }
+
+    public static int worldDayTime() {
+        return Minecraft.getInstance().level != null
+                ? (int) Minecraft.getInstance().level.getDayTime() : 0;
+    }
+
+    public static void narrate(String message) {
+        Minecraft.getInstance().execute(() -> {
+            Narrator.getNarrator().say(message, true);
+        });
+    }
+
+    public static void displayMessage(String message, boolean actionBar) {
+        final Player player = Minecraft.getInstance().player;
+        if (player != null) {
+            Minecraft.getInstance().execute(() -> {
+                player.displayClientMessage(Text.literal(message), actionBar);
+            });
+        }
+    }
+
+    public static void stopAllSounds() {
+        Minecraft.getInstance().execute(() -> {
+            Minecraft.getInstance().getSoundManager().stop();
+        });
+    }
+
+    public static void stopSound(SoundInstance sound) {
+        Minecraft.getInstance().execute(() -> {
+            Minecraft.getInstance().getSoundManager().stop(sound);
+        });
+    }
+
+    public static void stopSound(ResourceLocation sound, SoundSource source) {
+        Minecraft.getInstance().execute(() -> {
+            Minecraft.getInstance().getSoundManager().stop(sound, source);
+        });
+    }
+
+    public static void stopSound(ResourceLocation sound){
+        Minecraft.getInstance().execute(() -> {
+            Minecraft.getInstance().getSoundManager().stop(sound, SoundSource.BLOCKS);
+        });
+    }
+}
