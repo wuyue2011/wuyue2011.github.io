ea65e9d1e4d8a13daa2605ad3a196f1bf7120ad0
aphrodite281 <Aphrodite520@outlook.com>
2024-09-22 13:17:41 +0800

    ScriptDebugOverlay 添加自动换行

--- common/src/main/java/cn/zbx1425/mtrsteamloco/gui/ScriptDebugOverlay.java
+++ common/src/main/java/cn/zbx1425/mtrsteamloco/gui/ScriptDebugOverlay.java
@@ -48,32 +48,27 @@ public class ScriptDebugOverlay {
         for (Map.Entry<ScriptHolder, List<AbstractScriptContext>> entry : contexts.entrySet()) {
             ScriptHolder holder = entry.getKey();
             if (holder.failTime > 0) {
-                drawText(vdStuff, font, holder.name + " FAILED", 0, y, 0xFFFF0000);
-                y += lineHeight;
+                y = drawText(vdStuff, font, holder.name + " FAILED", 0, y, 0xFFFF0000);
                 for (String msgLine : Splitter.fixedLength(60).split(holder.failException.getMessage())) {
-                    drawText(vdStuff, font, msgLine, 5, y, 0xFFFF8888);
-                    y += lineHeight;
+                    y = drawText(vdStuff, font, msgLine, 5, y, 0xFFFF8888);
                 }
             } else {
-                drawText(vdStuff, font, holder.name, 0, y, 0xFFAAAAFF);
-                y += lineHeight;
+                y = drawText(vdStuff, font, holder.name, 0, y, 0xFFAAAAFF);
             }
             for (AbstractScriptContext context : entry.getValue()) {
-                drawText(vdStuff, font,
+                y = drawText(vdStuff, font,
                         String.format("#%08X (%.2f ms)", context.hashCode(), context.lastExecuteDuration / 1000.0),
                         10, y, 0xFFCCCCFF);
-                y += lineHeight;
                 for (Map.Entry<String, Object> debugInfo : context.debugInfo.entrySet()) {
                     Object value = debugInfo.getValue();
                     if (value instanceof GraphicsTexture) {
                         GraphicsTexture texture = (GraphicsTexture) value;
                         float scale = (Minecraft.getInstance().getWindow().getGuiScaledWidth() - 40) / (float) texture.width;
                         blit(vdStuff, texture.identifier, 20, y, (int)(texture.width * scale), (int)(texture.height * scale));
-                        drawText(vdStuff, font, debugInfo.getKey() + ": GraphicsTexture", 20, y, 0xFFFFFFFF);
-                        y += (int)(texture.height * scale) + lineHeight / 2;
+                        y = drawText(vdStuff, font, debugInfo.getKey() + ": GraphicsTexture", 20, y, 0xFFFFFFFF);
+                        y += (int)(texture.height * scale);
                     } else {
-                        drawText(vdStuff, font, debugInfo.getKey() + ": " + debugInfo.getValue(), 20, y, 0xFFFFFFFF);
-                        y += lineHeight;
+                        y = drawText(vdStuff, font, debugInfo.getKey() + ": " + debugInfo.getValue(), 20, y, 0xFFFFFFFF);
                     }
                 }
             }
@@ -84,15 +79,25 @@ public class ScriptDebugOverlay {
 
 
 #if MC_VERSION >= "12000"
-    private static void drawText(GuiGraphics guiGraphics, Font font, String text, int x, int y, int color) {
-        guiGraphics.drawString(font, text, x, y, color);
+    private static int drawText(GuiGraphics guiGraphics, Font font, String text, int x, int y, int color) {
+        List<String> lines = font.split(text, Minecraft.getInstance().getWindow().getGuiScaledWidth());
+        for (String line : lines) {
+            guiGraphics.drawString(font, line, x, y, color);
+            y += Mth.ceil(font.lineHeight * 1.2f);
+        }
+        return y;
     }
     private static void blit(GuiGraphics guiGraphics, ResourceLocation texture, int x, int y, int width, int height) {
         guiGraphics.blit(texture, x, y, width, height, 0, 0, 1, 1, 1, 1);
     }
 #else
-    private static void drawText(PoseStack matrices, Font font, String text, int x, int y, int color) {
-        font.drawShadow(matrices, text, x, y, color);
+    private static int drawText(PoseStack matrices, Font font, String text, int x, int y, int color) {
+        List<String> lines = font.split(text, Minecraft.getInstance().getWindow().getGuiScaledWidth());
+        for (String line : lines) {
+            font.drawShadow(matrices, line, x, y, color);
+            y += Mth.ceil(font.lineHeight * 1.2f);
+        }
+        return y;
     }
     private static void blit(PoseStack matrices, ResourceLocation texture, int x, int y, int width, int height) {
         RenderSystem.setShaderTexture(0, texture);
