d8c453695b368ac285ef1c53ce73105fd1dd4cee
aphrodite281 <Aphrodite520@outlook.com>
2024-09-26 21:53:19 +0800

    try

--- common/src/main/java/cn/zbx1425/mtrsteamloco/network/util/MapSerializer.java
+++ common/src/main/java/cn/zbx1425/mtrsteamloco/network/util/MapSerializer.java
@@ -1,9 +1,10 @@
 package cn.zbx1425.mtrsteamloco.network.util;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
