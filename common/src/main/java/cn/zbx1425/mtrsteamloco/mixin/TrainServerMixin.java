3077f945c3b76734dd29ccdcff0a4959f880b907
aphrodite281 <Aphrodite520@outlook.com>
2024-09-29 19:28:15 +0800

    try

--- common/src/main/java/cn/zbx1425/mtrsteamloco/mixin/TrainServerMixin.java
+++ common/src/main/java/cn/zbx1425/mtrsteamloco/mixin/TrainServerMixin.java
@@ -8,11 +8,13 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.Level;
 import cn.zbx1425.mtrsteamloco.block.BlockEyeCandy;
+import net.minecraft.world.phys.Vec3;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(TrainServer.class)
 public abstract class TrainServerMixin extends Train {
@@ -23,5 +25,37 @@ public abstract class TrainServerMixin extends Train {
 			final int doorStateValue = (int) Mth.clamp(doorValue * DOOR_MOVE_TIME, 0, 1);
 			((BlockEyeCandy.BlockEntityEyeCandy) entity).setOpen(doorStateValue);
 		}
+    }
+
+    @Inject(method = "scanDoors", at = @At("HEAD"), cancellable = true, remap = false)
+    private void onScanDoors(Level world, double trainX, double trainY, double trainZ, float checkYaw, float pitch, double halfSpacing, int dwellTicks, CallbackInfoReturnable<Boolean> ci) {
+        if (skipScanBlocks(world, trainX, trainY, trainZ)) {
+            ci.setReturnValue(false);
+			return;
+		}
+
+		boolean hasPlatform = false;
+		final Vec3 offsetVec = new Vec3(1, 0, 0).yRot(checkYaw).xRot(pitch);
+		final Vec3 traverseVec = new Vec3(0, 0, 1).yRot(checkYaw).xRot(pitch);
+
+		for (int checkX = 1; checkX <= 3; checkX++) {
+			for (int checkY = -2; checkY <= 3; checkY++) {
+				for (double checkZ = -halfSpacing; checkZ <= halfSpacing; checkZ++) {
+					final BlockPos checkPos = RailwayData.newBlockPos(trainX + offsetVec.x * checkX + traverseVec.x * checkZ, trainY + checkY, trainZ + offsetVec.z * checkX + traverseVec.z * checkZ);
+					final Block block = world.getBlockState(checkPos).getBlock();
+
+					if (block instanceof BlockPlatform || block instanceof BlockPSDAPGBase || block instanceof BlockEyeCandy) {
+						if (openDoors(world, block, checkPos, dwellTicks)) {
+                            ci.setReturnValue(true);
+							return;
+						}
+						hasPlatform = true;
+					}
+				}
+			}
+		}
+
+        ci.setReturnValue(hasPlatform);
+		return;
     }
 }
