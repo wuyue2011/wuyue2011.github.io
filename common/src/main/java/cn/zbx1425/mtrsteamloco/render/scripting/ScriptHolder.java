4904d03c68a43aed4b6c89e5833b638fa37eef3d
aphrodite281 <Aphrodite520@outlook.com>
2024-10-02 17:29:42 +0800

    ScriptHolder 添加 MTRRailwayData

--- common/src/main/java/cn/zbx1425/mtrsteamloco/render/scripting/ScriptHolder.java
+++ common/src/main/java/cn/zbx1425/mtrsteamloco/render/scripting/ScriptHolder.java
@@ -15,6 +15,7 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
 import vendor.cn.zbx1425.mtrsteamloco.org.mozilla.javascript.*;
+import mtr.data.RailwayData;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -82,6 +83,7 @@ public class ScriptHolder {
             scope.put("Vector3f", scope, new NativeJavaClass(scope, Vector3f.class));   
 
             scope.put("MTRClientData", scope, new NativeJavaClass(scope, ClientData.class));
+            scope.put("MTRRailwayData", scope, new NativeJavaClass(scope, RailwayData.class));
 
             scope.put("MinecraftClient", scope, new NativeJavaClass(scope, MinecraftClientUtil.class));
 
