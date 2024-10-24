f196ab31eb7ff24a361195eb75a87699564c73fc
aphrodite281 <Aphrodite520@outlook.com>
2024-10-24 21:38:29 +0800

    try

--- common/src/main/java/cn/zbx1425/mtrsteamloco/block/Propertys.java
+++ common/src/main/java/cn/zbx1425/mtrsteamloco/block/Propertys.java
@@ -3,5 +3,5 @@ package cn.zbx1425.mtrsteamloco.block;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 
 public class Propertys {
-    public static final IntegerProperty SHAPE = IntegerProperty.create("shape", 0, 1 <<< 30);
+    public static final IntegerProperty SHAPE = IntegerProperty.create("shape", 0, 1 << 30);
 }
