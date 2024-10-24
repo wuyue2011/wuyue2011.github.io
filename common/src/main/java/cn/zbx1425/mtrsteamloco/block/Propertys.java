762d4f71b511513df17953b7c2ab84ad58d94213
aphrodite281 <Aphrodite520@outlook.com>
2024-10-24 21:32:41 +0800

    push

---
+++ common/src/main/java/cn/zbx1425/mtrsteamloco/block/Propertys.java
@@ -0,0 +1,7 @@
+package cn.zbx1425.mtrsteamloco.block;
+
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+
+public class Propertys {
+    public static final IntegerProperty SHAPE = IntegerProperty.create("shape", 0, 1 <<< 30);
+}
