335780ff8a10896750f55d055012e72f9bd0f102
aphrodite281 <Aphrodite520@outlook.com>
2024-12-30 21:18:26 +0800

    push

--- build.ps1
+++ build.ps1
@@ -1,2 +1,2 @@
 $gradlewPath = "F:\mtr-nte\gradlew.bat"
-Start-Process -FilePath "$gradlewPath" -ArgumentList "build", "-PbuildVersion=1.18.2", "-Dorg.gradle.internal.http.socketTimeout=200000", "-Dorg.gradle.internal.http.connectionTimeout=200000", "--warning-mode all" -NoNewWindow -Wait
+Start-Process -FilePath "$gradlewPath" -ArgumentList "build", "-PbuildVersion=1.18.2", "--warning-mode all" -NoNewWindow -Wait
