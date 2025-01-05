e051fdee91f715c92308eb692d38b1cf9275db4b
aphrodite281 <Aphrodite520@outlook.com>
2025-01-05 17:11:10 +0800

    push

--- common/src/main/java/cn/zbx1425/mtrsteamloco/render/scripting/util/ComponentUtil.java
+++ common/src/main/java/cn/zbx1425/mtrsteamloco/render/scripting/util/ComponentUtil.java
@@ -1,19 +1,9 @@
 package cn.zbx1425.mtrsteamloco.render.scripting.util;
 
-import net.minecraft.network.chat.MutableComponent;
-import net.minecraft.network.chat.TextComponent;
-import net.minecraft.network.chat.TranslatableComponent;
+import mtr.mappings.Text;
 import net.minecraft.network.chat.Component;
 
-public interface ComponentUtil {
-
-	static MutableComponent translatable(String text, Object... objects) {
-		return new TranslatableComponent(text, objects);
-	}
-
-	static MutableComponent literal(String text) {
-		return new TextComponent(text);
-	}
+public interface ComponentUtil extends Text{
 
     static String getString(Component component) {
         return component.getString();
