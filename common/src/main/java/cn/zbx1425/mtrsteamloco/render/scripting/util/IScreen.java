fc28d37a52e5e2c2fbee1415c821f6693d22413e
aphrodite281 <Aphrodite520@outlook.com>
2025-02-01 22:29:48 +0800

    try

--- common/src/main/java/cn/zbx1425/mtrsteamloco/render/scripting/util/IScreen.java
+++ common/src/main/java/cn/zbx1425/mtrsteamloco/render/scripting/util/IScreen.java
@@ -39,15 +39,15 @@ public interface IScreen {
         }
     }
 
-# if MC_VERSION >= "12000"
+#if MC_VERSION >= "12000"
     public static Button newButton(int x, int y, int width, int height, Component text, Button.OnPress onPress) {
         return new Button(x, y, width, height, text, onPress);
     }
-# else
+#else
     public static Button newButton(int x, int y, int width, int height, Component text, Button.OnPress onPress) {
         return Button.Builder(text, onPress).pos(x, y).side(width, height).build();
     }
-# endif
+#endif
 
     public static class WithTextrue extends Screen {
         public GraphicsTexture texture;
