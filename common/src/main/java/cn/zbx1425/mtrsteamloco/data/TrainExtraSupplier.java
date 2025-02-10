6358ab22c4800447fc8f2ec491a06a997406ddb3
aphrodite281 <Aphrodite520@outlook.com>
2025-02-10 15:35:24 +0800

    调整滚转逻辑

--- common/src/main/java/cn/zbx1425/mtrsteamloco/data/TrainExtraSupplier.java
+++ common/src/main/java/cn/zbx1425/mtrsteamloco/data/TrainExtraSupplier.java
@@ -24,8 +24,13 @@ public interface TrainExtraSupplier {
     static float getRollAngleAt(Train train, int car) {
         // double p = train.getRailProgress() - car * train.spacing - train.spacing / 2;
         // return ((TrainExtraSupplier) train).getRollAngleAt(p);
-        double p1 = train.getRailProgress() - car * train.spacing;
-        double p2 = p1 - train.spacing;
+        boolean b = train.isReversed();
+        int c1 = (b ? train.trainCars - car : car);
+        car += 1;
+        int c2 = (b ? train.trainCars - car : car);
+        double p = train.getRailProgress();
+        double p1 = p - c1 * train.spacing;
+        double p2 = p - c2 * train.spacing;
         TrainExtraSupplier supplier = (TrainExtraSupplier) train;
         return (supplier.getRollAngleAt(p1) + supplier.getRollAngleAt(p2)) / 2;
     }
