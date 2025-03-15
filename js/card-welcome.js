8e4c1b04fa6e4b577a2accc46b3c486e4e7e0103
“wuyue2011” <chenjianan20110607@outlook.com>
2025-03-15 11:54:48 +0800

    Site updated: 2025-03-15 11:54:47

--- js/card-welcome.js
+++ js/card-welcome.js
@@ -1,8 +1,8 @@
 window.IP_CONFIG = {
 	API_KEY: 'CF40OWzXZN5XZZaaOCzQSHKvXw', // API密钥 申请地址：https://api.76.al/
 	BLOG_LOCATION: {
-		lng: 113.666, // 经度
-		lat: 34.666 // 纬度
+		lng: 118.16, // 经度
+		lat: 25.05 // 纬度
 	},
 	CACHE_DURATION: 1000 * 60 * 60, // 可配置缓存时间(默认1小时)
 	HOME_PAGE_ONLY: true, // 是否只在首页显示 开启后其它页面将不会显示这个容器
