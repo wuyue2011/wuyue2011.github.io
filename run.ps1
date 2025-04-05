c97fa500fad65388c922793786f552bf1e034f97
aphrodite281 <Aphrodite520@outlook.com>
2025-04-05 20:02:35 +0800

    调整逻辑0

--- run.ps1
+++ run.ps1
@@ -1,6 +1,15 @@
-$sourcePath = "F:\mtr-ante\build\MTR-ANTE-1.0.4+1.18.2.jar"
-$destinationPath = "F:\mc\araf\versions\1.18.2-Fabric 0.16.9\mods"
-$executablePath = "F:\mc\run.bat"
+$bf = "F:\mtr-ante\build"
+$dp = "F:\mc\araf\versions\1.18.2-Fabric 0.16.9\mods"
+$ep = "F:\mc\run.bat"
 
-Copy-Item -Path "$sourcePath" -Destination "$destinationPath" -Recurse -Force
-Start-Process -FilePath powershell.exe -ArgumentList "-Command `& `'$executablePath`'"
+$sp = Get-ChildItem -Path $bf -Filter "*1.18.2.jar" | 
+      Sort-Object LastWriteTime -Descending | 
+      Select-Object -First 1 -ExpandProperty FullName
+
+if ( $sp ) {
+    Remove-Item -Path "$dp\MTR-ANTE*" -Force
+    Copy-Item -Path "$sp" -Destination "$dp" -Recurse -Force
+    Start-Process -FilePath powershell.exe -ArgumentList "-Command `& `'$ep`'"
+} else {
+    Write-Host "No new build found."
+}
